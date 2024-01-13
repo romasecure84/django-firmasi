9a2ab890d579a2d05fd61cd2e172a0d10310862e
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-13 20:16:45 +0400

    templates yaradildi

--- page/views.py
+++ page/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import randint
 
 def home(request):
-    return HttpResponse('Ana Sehifeye Xos Geldiniz!')
-# Create your views here.
+    context = {'platform': f'Django Platformu Istifade Edildi: {randint(1, 100)} '}
+    return render(request, 'page/home_page.html', context)
+
