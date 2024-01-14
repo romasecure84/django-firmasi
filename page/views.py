ca4627c2fca8276ebaa01e2c1350cd808a91ca54
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-15 00:58:20 +0400

    Tittle block halina getirildi

--- page/views.py
+++ page/views.py
@@ -4,17 +4,21 @@ from random import randint
 
 def home_view(request):
     # context = {'platform': f'Django Platformu Istifade Edildi: {randint(1, 100)} '}
-    context = {}
+    page_title = 'Ana Sehife'
+    context = dict(page_title = page_title)
     return render(request, 'page/home_page.html', context)
 
 def about_us_view(request):
-    context = dict()
+    page_title = 'Haqqimizda'
+    context = dict(page_title = page_title,)
     return render(request, 'page/about_us_view.html', context)
 
 def contact_us_view(request):
-    context = dict()
+    page_title = 'Elaqe'
+    context = dict(page_title = page_title,)
     return render(request, 'page/contact_us_view.html', context)
 
 def vision_view(request):
-    context = dict()
+    page_title = 'Vizyonumuz'
+    context = dict(page_title = page_title,)
     return render(request, 'page/vision.html', context)
