fbb380b58c68e14d91e284fdfee7c9072e95421f
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-01-17 20:03:04 +0400

    global context yaradildi ve settings.py vasitesiyle butun sehifelere projets bolumu elave edildi

--- page/views.py
+++ page/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse, Http404
 from random import randint
 from .fake_db.pages import FAKE_DB_PAGES
 
-FAKE_DB_PROJECTS = [
-    f"https://picsum.photos/id/{id}/100/80"  for id in range(121, 129) 
-]
+#FAKE_DB_PROJECTS = [
+#    f"https://picsum.photos/id/{id}/100/80"  for id in range(121, 129) 
+#]
 
 FAKE_DB_CAROUSEL = [
     f"https://picsum.photos/id/{id}/1200/400" for id in range(11, 16)
@@ -15,7 +15,7 @@ def home_view(request):
     # context = {'platform': f'Django Platformu Istifade Edildi: {randint(1, 100)} '}
     page_title = 'Ana Sehife'
     context = dict(page_title = page_title,
-                    FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
+                    #FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
                     FAKE_DB_CAROUSEL = FAKE_DB_CAROUSEL,)
     return render(request, 'page/home_page.html', context)
 
@@ -24,7 +24,7 @@ def about_us_view(request):
     hero_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum, metus a euismod bibendum, libero urna hendrerit diam, sit amet ornare nibh ligula non arcu. Vivamus rutrum urna quis ex fermentum cursus. Aliquam varius erat sem, sit amet pellentesque ipsum venenatis non. Phasellus dapibus, ante posuere fermentum tristique, turpis libero iaculis arcu, eget gravida erat sem sit amet nulla. Nullam porttitor, nulla ut ultricies scelerisque, enim nulla laoreet metus, blandit lacinia turpis eros eget mauris. Nullam at egestas dolor. Quisque sagittis mattis commodo."
     context = {"page_title": page_title, 
             "hero_content": hero_content,
-             "FAKE_DB_PROJECTS" : FAKE_DB_PROJECTS,
+             #"FAKE_DB_PROJECTS" : FAKE_DB_PROJECTS,
     }
     return render(request, 'page/about_us_view.html', context)
 
@@ -33,14 +33,14 @@ def contact_us_view(request):
     hero_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum, metus a euismod bibendum, libero urna hendrerit diam, sit amet ornare nibh ligula non arcu. Vivamus rutrum urna quis ex fermentum cursus. Aliquam varius erat sem, sit amet pellentesque ipsum venenatis non. Phasellus dapibus, ante posuere fermentum tristique, turpis libero iaculis arcu, eget gravida erat sem sit amet nulla. Nullam porttitor, nulla ut ultricies scelerisque, enim nulla laoreet metus, blandit lacinia turpis eros eget mauris. Nullam at egestas dolor. Quisque sagittis mattis commodo."
     context = dict(page_title = page_title, 
                    hero_content = hero_content,
-                   FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
+                   #FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
     )
     return render(request, 'page/contact_us_view.html', context)
 
 def vision_view(request):
     page_title = 'Vizyonumuz'
     context = dict(page_title = page_title,
-                    FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
+                    #FAKE_DB_PROJECTS = FAKE_DB_PROJECTS,
                     )
     return render(request, 'page/vision.html', context)
 
@@ -50,7 +50,7 @@ def page_view(request, slug):
     if result:
         context = dict(
             page_title = result[0]['title'],
-            FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
+            #FAKE_DB_PROJECTS=FAKE_DB_PROJECTS,
             detail= result[0]['detail'],
         )
         return render(request, "page/page_detail.html", context)
--- product/views.py
+++ product/views.py
@@ -3,15 +3,19 @@ from django.shortcuts import render
 from .fake_db.products import FAKE_DB_PRODUCTS
 
 def product_list_view(request):
+    page_title = 'Products'
     context = dict(
+        page_title = page_title,
         products = FAKE_DB_PRODUCTS,
     )
     return render(request, 'product/products.html', context)
 
 def product_detail_view(request, id):
+    #page_title = 'Product'
     result = list(filter(lambda x:(x['id'] == id), FAKE_DB_PRODUCTS))
     if result:
         context = dict(
+            page_title = 'Product_id',
             item = result[0],
             products=FAKE_DB_PRODUCTS,
             detail= result[0]['detail'],
